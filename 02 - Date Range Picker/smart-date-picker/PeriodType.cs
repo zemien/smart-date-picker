27de29d9f3ae162c849c7638e7f0eb155eae3885
Shawn <zemien85@gmail.com>
2016-07-23 22:45:37 +1200

    Initial clone of 01 project to begin with

---
+++ 02 - Date Range Picker/smart-date-picker/PeriodType.cs
@@ -0,0 +1,23 @@
+﻿namespace smart_date_picker
+{
+    /// <summary>
+    /// Report period type
+    /// </summary>
+    public enum PeriodType
+    {
+        /// <summary>
+        /// By week
+        /// </summary>
+        Weeks,
+
+        /// <summary>
+        /// By month
+        /// </summary>
+        Months,
+
+        /// <summary>
+        /// By calendar quarter
+        /// </summary>
+        Quarters
+    }
+}
